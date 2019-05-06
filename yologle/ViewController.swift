9062edaa9801775244ea832ffe80f28604376fb8
mobileflowllc <fieldforceapp@gmail.com>
2019-05-06 15:28:37 -0700

    Initial Commit

---
+++ yologle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yologle
+//
+//  Created by d. nye on 5/6/19.
+//  Copyright © 2019 Mobile Flow LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
